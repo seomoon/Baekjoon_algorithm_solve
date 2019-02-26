add82def133795cd19d562c74dcb1a214106a70b
seomoon <tjans1457@naver.com>
2019-02-26 12:44:30 +0900

    5014번

---
+++ 스타트링크.cpp
@@ -0,0 +1,57 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+int f, s, g, u, d;
+vector<bool> visit;
+int result;
+void bfs(int x) {//±‚¡ÿ ∞™
+	visit[s] = true;
+	queue<pair<int, int>> q; //¡¬«• ¡Àº“¿Ãµø
+	q.push(make_pair(s, 0));
+	while (!q.empty()) {
+		pair<int, int> cur = q.front();
+		q.pop();
+		
+		int n_u = cur.first + u;
+		int n_d = cur.first - d;
+		if (n_u == g || n_d == g) {
+			result = cur.second;
+			return;
+		}
+		if (n_d >= 1) {
+			if (!visit[n_d]) {
+				visit[n_d] = true;
+				q.push(make_pair(n_d, cur.second+1));
+			}
+		}
+		if (n_u <= f) {
+			if (!visit[n_u]) {
+				visit[n_u] = true;
+				q.push(make_pair(n_u, cur.second + 1));
+			}
+		
+		}
+		
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> f >> s >> g >> u >> d;
+	visit = vector<bool>(f+1);
+	bfs(s);
+	if (s == g) {
+		cout << 0;
+	}
+	else if (result ==0) {
+		cout << "use the stairs";
+	}
+	else {
+		cout << result+1;
+	}
+	
+	return 0;
+}
